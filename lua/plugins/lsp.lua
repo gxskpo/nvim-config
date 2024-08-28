83afdf5aae63c0a404382e806e4b369c7747f25d
Haruka <hello@hawruka.de>
2024-08-27 23:33:35 -0600

    Add lsp zero plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,18 +3,14 @@ return {
         "neovim/nvim-lspconfig",
         config = function() require("plugins.lsp") end
     }, {
-         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup()
-        end
+      "VonHeikemen/lsp-zero.nvim", 
+      branch = "v3.x"
     }, {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline", "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip"
-        }
+	}
     }
 }
